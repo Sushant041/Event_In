eeff06f4cbd55a89ed4648934b358e56981bc1e0
Sushsnat <grtsushant@gmail.com>
2024-05-12 21:00:23 +0530

    new

--- src/lib/actions/orderAction.tsx
+++ src/lib/actions/orderAction.tsx
@@ -110,7 +110,7 @@ export async function getOrdersByEvent({ searchString, eventId }: GetOrdersByEve
 
     return JSON.parse(JSON.stringify(orders))
   } catch (error) {
-    handleError(error)
+    handleError(error);
   }
 }
 
